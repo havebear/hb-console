7275285095b1ec4cc1611c23a7229927f8e64070
havebear <616749285@qq.com>
2022-04-22 17:56:01 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: BGG
+ * @Date: 2022-04-22 17:55:31
+ * @LastEditors: BGG
+ * @LastEditTime: 2022-04-22 17:55:33
+ * @Description:  
+ */
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: BGG
+ * @Date: 2022-04-22 17:55:39
+ * @LastEditors: BGG
+ * @LastEditTime: 2022-04-22 17:55:39
+ * @Description:  
+ */
